48abebd392c36a06458ce7f0ce0219de5af7b907
gogins <michael.gogins@gmail.com>
2022-10-17 16:50:45 -0400

    Access to normalized value of StrangeAttractor.

--- update-dependency-submodules.sh
+++ update-dependency-submodules.sh
@@ -3,4 +3,9 @@ echo "Updating all submodules for csound-wasm..."
 git submodule update --init --recursive --remote
 git submodule update --recursive
 git submodule status --recursive
+cd dependencies/csound-ac
+git checkout master
+git branch
+cd ../..
+pwd
 echo "Finished updating all submodules for csound-wasm."
