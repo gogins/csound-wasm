5cae134e98ebbe584f1a442adcde65a5efdd849d
gogins <michael.gogins@gmail.com>
2022-10-28 10:53:21 -0400

    Updated Csound, improved build scripts.

--- update-dependency-submodules.sh
+++ update-dependency-submodules.sh
@@ -6,6 +6,12 @@ git submodule status --recursive
 cd dependencies/csound-ac
 git checkout master
 git branch
+git pull
+cd ../..
+cd dependencies/csound
+git checkout master
+git branch
+git pull
 cd ../..
 pwd
 echo "Finished updating all submodules for csound-wasm."
