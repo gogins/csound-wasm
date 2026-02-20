18e09fd1c8b2f8c49248a7842600dae457e671b3
gogins <michael.gogins@gmail.com>
2026-02-20 12:22:11 +0100

    Force fetching from submodule tip.

--- update-dependency-submodules.sh
+++ update-dependency-submodules.sh
@@ -1,17 +1,32 @@
 #!/bin/bash 
 echo "Updating all submodules for csound-wasm..."
-git submodule update --init --recursive --remote
-git submodule update --recursive
-git submodule status --recursive
-cd dependencies/csound-ac
-git checkout chord-space-gluing
-git branch
-git pull
-cd ../..
-cd dependencies/csound
-git checkout csound6
-git branch
-git pull
-cd ../..
+
+git submodule update --init --recursive
+
+(
+    cd dependencies/csound-ac
+
+    # Make sure origin exists and fetch the branch tip
+    git remote -v
+    git fetch --prune origin +refs/heads/chord-space-gluing:refs/remotes/origin/chord-space-gluing
+
+    # Create/update local branch to point at the remote branch tip
+    git checkout -B chord-space-gluing origin/chord-space-gluing
+
+    # Hard reset to ensure you are exactly at the latest remote commit
+    git reset --hard origin/chord-space-gluing
+
+    git status
+    git log -1 --oneline
+)
+
+(
+    cd dependencies/csound
+    git fetch --prune origin
+    git checkout -B csound6 origin/csound6
+    git reset --hard origin/csound6
+    git log -1 --oneline
+)
+
 pwd
 echo "Finished updating all submodules for csound-wasm."
